6b60aa168014a6827776a2ff34de524090e25d5d
Manuel DUDDA <manuel@dudda-und-dudda.de>
2015-11-06 19:38:08 +0100

    Update minisat3.rb

--- Library/Formula/minisat3.rb
+++ Library/Formula/minisat3.rb
@@ -15,8 +15,10 @@ class Minisat3 < Formula
   def install
     ENV["MROOT"] = buildpath
     system "make", "-C", "core", "r"
+    puts bin
+    puts lib
     bin.install "core/minisat_release" => "minisat"
-    lib.install 'core/libminisat_release.a' => 'libminisat.a'
+    #lib.install 'core/libminisat_release.a' => 'libminisat.a'
     lib.install 'core/libminisat.dylib'
   end
 end
